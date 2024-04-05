571f10183d2dc5fead0c74f65799c2a11b9abd5c
Kcuong93 <146831233+Kcuong93@users.noreply.github.com>
2024-04-05 08:56:23 +0700

    Update t.js

--- t.js
+++ t.js
@@ -1,41 +1,33 @@
-function statusChangeCallback(response) {  
+function statusChangeCallback(response) {
     console.log('statusChangeCallback');
-    console.log(response);                   
-    if (response.status === 'connected') {   
-      testAPI();  
-    } else {                                 
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else {
+      // The person is not logged into your app or we are unable to tell.
       document.getElementById('status').innerHTML = 'Please log ' +
-        'into this webpage.';
+        'into this app.';
     }
-}
+  }
 
-
-function checkLoginState() {              
-    FB.getLoginStatus(function(response) {   
-        statusChangeCallback(response);
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
     });
-}
-
-
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '24989476977363948',
-        cookie     : true,                     
-        xfbml      : true,                  
-        version    : 'v19.0'          
-    });
-
-
-    FB.getLoginStatus(function(response) {   
-        statusChangeCallback(response);       
-    });
-};
-
-function testAPI() {                      
+  }
+  function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
-        console.log('Successful login for: ' + response.name);
-        document.getElementById('status').innerHTML =
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
     });
-}
+  }
