7de4520f828d650b7cd0047ace32a8d5e17821b2
Kcuong93 <146831233+Kcuong93@users.noreply.github.com>
2024-04-05 23:56:39 +0700

    Update t.js

--- t.js
+++ t.js
@@ -1,42 +1,42 @@
- window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '3948960068721135',
-    cookie     : true,  
-    xfbml      : true, 
-    version    : 'v19.0'
-  });
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '3948960068721135',
+        cookie     : true,  
+        xfbml      : true, 
+        version    : 'v19.0'
+    });
 
 
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
 
-  };
+};
+document.getElementById('_e').addEventListener('click', () => {
+    checkLoginState()
+})
 function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
+ 
     console.log(response);
 
     if (response.status === 'connected') {
-
-      testAPI();
+        testAPI();
     } else {
-
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
+        document.getElementById('status').innerHTML = 'Please log into this app.';
     }
-  }
+}
 
 
-  function checkLoginState() {
+function checkLoginState() {
     FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
+        statusChangeCallback(response);
     });
-  }
-  function testAPI() {
+}
+
+function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
     });
-  }
+}
